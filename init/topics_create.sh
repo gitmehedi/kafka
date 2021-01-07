95783b2847112f9f411740e552f8b626acbb9e4d
gitmehedi <git.mehedi@gmail.com>
2021-01-07 18:43:31 +0600

    [MOD] Modify on consumer and producer

--- init/topics_create.sh
+++ init/topics_create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/kafka/kafka/bin/kafka-topics.sh --zookeeper localhost:2182 --create --topic cassandra --partitions 10 --replication-factor 3
 
-/kafka/kafka/bin/kafka-server-start.sh /kafka/kafka/config/server.properties
+/kafka/kafka/bin/kafka-topics.sh --create --zookeeper 192.168.56.22:2181 --partitions 3 --replication-factor 3 --topics cassandra
+/kafka/kafka/bin/kafka-server-start.sh /kafka/kafka/config/server-101.properties
