b7376af28470a9f33ecd5693e980a78211fa4d50
gitmehedi <git.mehedi@gmail.com>
2021-01-10 09:05:49 +0600

    [MOD] Modified on neo4j connect.

--- init/neo_coonect.py
+++ init/neo_coonect.py
@@ -6,22 +6,21 @@ pg = utils.DbConnect('postgres')
 driver = GraphDatabase.driver("bolt://192.168.56.22:7687")
 
 
-def add_friend(tx, name, friend_name):
-    tx.run("MERGE (a:Person {name: $name}) "
-           "MERGE (a)-[:KNOWS]->(friend:Person {name: $friend_name})",
-           name=name, friend_name=friend_name)
+def add_friend(tx, branch, user):
+    tx.run("MERGE (a:Branch {name: $branch}) "
+           "MERGE (a)-[:KNOWS]->(user:User {name: $user})",
+           branch=branch, user=user)
 
 
 def print_friends(tx, name):
-    for record in tx.run("MATCH (a:Person)-[:KNOWS]->(friend) WHERE a.name = $name "
-                         "RETURN friend.name ORDER BY friend.name", name=name):
-        print(record["friend.name"])
+    for record in tx.run("match (n) return n"):
+        print(record)
 
 
 connection = psycopg2.connect(user=pg['username'], password=pg['password'], host=pg['hostname'], port=pg['port'],
                               database=pg['database'])
 cursor = connection.cursor()
-cursor.execute("SELECT ru.login,ou.name from res_users ru \
+cursor.execute("SELECT ou.name,ru.login from res_users ru \
                 LEFT JOIN operating_unit ou \
                 ON (ou.id = ru.default_operating_unit_id) \
                 WHERE ou.name IS NOT NULL \
@@ -31,6 +30,5 @@ with driver.session() as session:
     for val in cursor.fetchall():
         print("User Name:{0}, Branch: {1}".format(val[0], val[1]))
         session.write_transaction(add_friend, val[0], val[1])
-        session.read_transaction(print_friends, val[0])
 
 driver.close()
